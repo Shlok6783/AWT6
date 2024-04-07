5918f2eef8d6365e7db87bc0d8fe30ac0d04757f
Shlok6783 <shlok.sanghvi06783@gmail.com>
2024-04-07 21:29:53 +0530

    Initial commit with application setup 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/my-api', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'))
